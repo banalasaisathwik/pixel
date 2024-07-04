d819997eafb4208397d667ceb1ddf322fda766ca
banala sai sathwik <banalasaisathwik@gmail.com>
2024-07-04 22:39:21 +0530

    mobile size and push changed

--- src/components/ImageMap.tsx
+++ src/components/ImageMap.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 const ImageMap: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [mapLoaded, setMapLoaded] = useState<boolean>(false);
     const [isExpanded, setIsExpanded] = useState<boolean | null>(null);
     const [tooltip, setTooltip] = useState<{ visible: boolean; content: string; x: number; y: number; }>({ visible: false, content: '', x: 0, y: 0 });
-
     const router = useRouter();
 
     useEffect(() => {
@@ -27,7 +26,7 @@ const ImageMap: React.FC = () => {
 
     useEffect(() => {
         const sessionIsExpanded = sessionStorage.getItem('isExpanded');
-        setIsExpanded(sessionIsExpanded === 'true' ? true : false);
+        setIsExpanded(sessionIsExpanded === 'true');
     }, []);
 
     const handlePixelClick = (row: number, col: number): void => {
@@ -35,7 +34,7 @@ const ImageMap: React.FC = () => {
     };
 
     const toggleExpand = (): void => {
-        sessionStorage.setItem('isExpanded', 'true')
+        sessionStorage.setItem('isExpanded', (!isExpanded).toString());
         setIsExpanded(prevState => !prevState);
     };
 
@@ -55,26 +54,24 @@ const ImageMap: React.FC = () => {
     const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
         if (isExpanded && mapLoaded) {
             const rect = e.currentTarget.getBoundingClientRect();
-            const scaleX = e.currentTarget.width / rect.width;   // Determine the scale factor for X
-            const scaleY = e.currentTarget.height / rect.height; // Determine the scale factor for Y
+            const scaleX = e.currentTarget.width / rect.width;
+            const scaleY = e.currentTarget.height / rect.height;
 
-            // Adjust mouse position by the canvas position and scale
             const x = (e.clientX - rect.left) * scaleX;
             const y = (e.clientY - rect.top) * scaleY;
 
             const row = Math.floor(y / 10);
             const col = Math.floor(x / 10);
 
-            // Update tooltip position to follow the mouse. Adjust as needed for offset.
-            // Here, we're using the unscaled mouse positions for simplicity.
             setTooltip({
                 visible: true,
                 content: `Pixel: (${row}, ${col}) - Click here for more details`,
-                x: e.clientX - rect.left, // Position tooltip relative to canvas, not viewport
+                x: e.clientX - rect.left,
                 y: e.clientY - rect.top
             });
         }
     };
+
     const handleMouseLeave = () => {
         setTooltip(prev => ({ ...prev, visible: false }));
     };
@@ -91,64 +88,95 @@ const ImageMap: React.FC = () => {
     };
 
     return (
-        <motion.div
-            initial={false}
-            animate={isExpanded !== null ? (isExpanded ? 'expanded' : 'normal') : undefined}
-            variants={variants}
-            className='cursor-pointer  w-full min-h-screen overflow-y-auto'
-            onClick={toggleExpand}
-        >
-
-            <canvas
-                className={`cursor-pointer ${isExpanded ? 'mt-[200px]' : 'absolute top-1/2 translate-y-[-40%] left-1/2 translate-x-[-50%] w-[60%] lg:w-[20%] '}  transition-all duration-300`}
-                ref={canvasRef}
-                width={2000}
-                height={2000}
-                onClick={(e) => {
-                    if (isExpanded && mapLoaded) {
-                        const rect = e.currentTarget.getBoundingClientRect();
-                        const x = e.clientX - rect.left;
-                        const y = e.clientY - rect.top;
-                        const row = Math.floor(y / 10);
-                        const col = Math.floor(x / 10);
-                        handlePixelClick(row, col);
-                    }
-                }}
-                onMouseMove={handleMouseMove}
-                onMouseLeave={handleMouseLeave}
-
-            />
-            {tooltip.visible && canvasRef.current && (
-                <div
-                    style={{
-                        position: 'absolute',
-                        top: `${tooltip.y + canvasRef.current.offsetTop + 15}px`,
-                        left: `${tooltip.x + canvasRef.current.offsetLeft + 15}px`,
-                        backgroundColor: 'white',
-                        padding: '5px',
-                        borderRadius: '5px',
-                        boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
-                        zIndex: 100,
+        <>
+            {/* Desktop View */}
+            <motion.div
+                initial={false}
+                animate={isExpanded !== null ? (isExpanded ? 'expanded' : 'normal') : undefined}
+                variants={variants}
+                className='cursor-pointer hidden md:block w-full min-h-screen overflow-y-auto'
+                onClick={toggleExpand}
+            >
+                <canvas
+                    className={`cursor-pointer ${isExpanded ? 'mt-[200px]' : 'absolute top-1/2 translate-y-[-40%] left-1/2 translate-x-[-50%] w-[60%] lg:w-[20%]'} transition-all duration-300`}
+                    ref={canvasRef}
+                    width={2000}
+                    height={2000}
+                    onClick={(e) => {
+                        if (isExpanded && mapLoaded) {
+                            const rect = e.currentTarget.getBoundingClientRect();
+                            const x = e.clientX - rect.left;
+                            const y = e.clientY - rect.top;
+                            const row = Math.floor(y / 10);
+                            const col = Math.floor(x / 10);
+                            handlePixelClick(row, col);
+                        }
                     }}
-                >
-                    {tooltip.content}
-                </div>
-            )}
+                    onMouseMove={handleMouseMove}
+                    onMouseLeave={handleMouseLeave}
+                />
+                {tooltip.visible && canvasRef.current && (
+                    <div
+                        style={{
+                            position: 'absolute',
+                            top: `${tooltip.y + canvasRef.current.offsetTop + 15}px`,
+                            left: `${tooltip.x + canvasRef.current.offsetLeft + 15}px`,
+                            backgroundColor: 'white',
+                            padding: '5px',
+                            borderRadius: '5px',
+                            boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
+                            zIndex: 100,
+                        }}
+                    >
+                        {tooltip.content}
+                    </div>
+                )}
 
+                {!isExpanded && (
+                    <p className="text-gray-700 absolute top-1/2 left-[50%] transform translate-x-[-100%] animate-pulse -translate-y-[-0%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-14 h-14">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672L13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+                        </svg>
+                    </p>
+                )}
 
-            {!isExpanded && (
-                <p className="text-gray-700 absolute top-1/2 left-[50%] transform translate-x-[-100%] animate-pulse -translate-y-[-0%]">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-14 h-14">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
-                    </svg>
+                <button className="absolute bottom-10 right-10 bg-white text-gray-900 px-4 py-2 rounded shadow" onClick={downloadMap}>
+                    Download Map
+                </button>
+            </motion.div>
 
-                </p>
-            )}
+            {/* Mobile View */}
+            <div className='cursor-pointer md:hidden flex justify-center items-center w-full min-h-screen overflow-y-auto'>
+                <canvas
+                    className='cursor-pointer w-full h-full object-contain transition-all duration-300'
+                    ref={canvasRef}
+                    width={2000}
+                    height={2000}
+                    onClick={() => {
+                        void router.push(`/pixel?row=0&col=0`);
+                    }}
+                    onMouseMove={handleMouseMove}
+                    onMouseLeave={handleMouseLeave}
+                />
+                {tooltip.visible && canvasRef.current && (
+                    <div>
+                        {tooltip.content}
+                    </div>
+                )}
 
-            <button className="absolute bottom-10 right-10 bg-white text-gray-900 px-4 py-2 rounded shadow" onClick={downloadMap}>
-                Download Map
-            </button>
-        </motion.div>
+                {!isExpanded && (
+                    <p className="text-gray-700 absolute top-1/2 left-[50%] transform translate-x-[-100%] animate-pulse -translate-y-[-0%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-14 h-14">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672L13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+                        </svg>
+                    </p>
+                )}
+
+                <button className="absolute bottom-10 right-10 bg-white text-gray-900 px-4 py-2 rounded shadow" onClick={downloadMap}>
+                    Download Map
+                </button>
+            </div>
+        </>
     );
 };
 
--- src/pages/pixel.tsx
+++ src/pages/pixel.tsx
@@ -1,14 +1,15 @@
-
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { api } from '~/utils/api';
 import Image from 'next/image';
 import Loading from '~/components/Loading';
 import Link from 'next/link';
+
 const Hello = () => {
     const [visitorsCount, setVisitorsCount] = useState(0);
     const router = useRouter();
     const { row, col } = router.query;
+
     const rowNumber = typeof row === 'string' ? parseInt(row, 10) : undefined;
 
     const { data: websiteDetails, isLoading, isError } = api.details.retrive.useQuery({
@@ -16,7 +17,7 @@ const Hello = () => {
         col: parseInt(col as string, 10)
     });
 
-    const { mutate: updateVisitorsCount , data } = api.details.visitors.useMutation({
+    const { mutate: updateVisitorsCount, data } = api.details.visitors.useMutation({
         onSuccess: (data) => {
             if (data.visitors !== null) {
                 setVisitorsCount(data.visitors);
@@ -30,27 +31,47 @@ const Hello = () => {
         }
     }, [websiteDetails, visitorsCount, updateVisitorsCount]);
 
-    if (isLoading) {
-        return <div className="flex justify-center items-center h-screen">
-            <div className="text-lg font-semibold">Loading...</div>
-        </div>;
-    }
-
-    if (isError) {
+    if (row === "0" && col === "0") {
         return (
-            <div className="mx-auto p-4 cursor-pointer  bg-cover bg-center w-full min-h-screen">
-            <div className="flex justify-center items-center h-screen ">
+            <div className="mx-auto p-4 cursor-pointer bg-cover bg-center w-full min-h-screen">
                 <div className="p-8 mt-20 text-center">
-                        <h2 className="text-6xl text-white   font-bold mb-4">{"For sale! If it's on the map."}</h2>
-                    <p className="text-xl  text-gray-100 my-6">The block has not yet been sold.</p>
-                  
-                    <Link href={"/buyer/info"} className='text-white cursor-pointer text-xl bg-black p-4'>Want to buy this block ?</Link>
+                    <p className="text-xl text-gray-100 my-6">Sorry!! This feature is available only on laptop or desktop.</p>
+                    <p className='text-white text-xl bg-black p-4'>{"But here is the demo on how it's shown on laptop or desktop 👇"}</p>
+                </div>
+                <div className="max-w-7xl mx-auto lg:p-6 lg:mt-44 w-full">
+                    <div className='w-full justify-between flex flex-col lg:flex-row items-center'>
+                        <div className='flex flex-col lg:flex-row gap-6 w-full justify-between'>
+                            <div className='flex gap-4 lg:gap-10 h-full'>
+                                <Image width={100} height={100} className='lg:w-32 lg:h-32 w-20 h-20 object-cover object-center rounded-2xl' src="/logo.png" alt='' />
+                                <div className='flex flex-col items-center'>
+                                    <h1 className='text-3xl lg:text-6xl font-sai my-2 text-white'>
+                                        <span className='font-kumar text-yellow-600'>#</span>
+                                        {"Your website name appears here"}
+                                    </h1>
+                                    <p className='text-2xl hidden lg:block text-white font-light'>{"Your website tagline appears here"}</p>
+                                </div>
+                            </div>
+                            <p className='text-2xl lg:hidden text-white font-light'>{"Your website tagline appears here"}</p>
+                            <div className='flex flex-col lg:flex-row items-center gap-4'>
+                                <div className='lg:w-[240px] w-full justify-center text-xl flex items-center gap-4 font-semibold rounded-md border-2 border-white text-white p-4'>
+                                    <p>Visits: {"Displays the count of clicks on your ad"}</p>
+                                </div>
+                                <Link href={'/about'} className='text-xl font-semibold w-full lg:w-[200px] text-center rounded-md bg-white text-gray-900 p-4 font-sai'>Visit Website</Link>
+                            </div>
+                        </div>
+                    </div>
+                    <hr className='w-full mt-10' />
+                    <div className='my-10'>
+                        <h1 className='text-white font-sai text-3xl'>What is {"(Your website name appears here)"}?</h1>
+                        <p className='py-4 text-white text-xl whitespace-pre-line'>
+                            {"Your website description appears here"}
+                        </p>
+                    </div>
                 </div>
             </div>
-            </div>
         );
-
     }
+
     if (isLoading) {
         return (
             <div className="flex justify-center items-center h-screen">
@@ -59,33 +80,46 @@ const Hello = () => {
         );
     }
 
+    if (isError) {
+        return (
+            <div className="mx-auto p-4 cursor-pointer bg-cover bg-center w-full min-h-screen">
+                <div className="flex justify-center items-center h-screen">
+                    <div className="p-8 mt-20 text-center">
+                        <h2 className="text-6xl text-white font-bold mb-4">{"For sale! If it's on the map."}</h2>
+                        <p className="text-xl text-gray-100 my-6">The block has not yet been sold.</p>
+                        <Link href={"/buyer/info"} className='text-white cursor-pointer text-xl bg-black p-4'>Want to buy this block?</Link>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
-        <div className="mx-auto p-4 cursor-pointer  bg-cover bg-center w-full min-h-screen">
+        <div className="mx-auto p-4 cursor-pointer bg-cover bg-center w-full min-h-screen">
             <div className="max-w-7xl mx-auto lg:p-6 mt-44 lg:mt-44 w-full">
                 <div className='w-full justify-between flex flex-col lg:flex-row items-center'>
-                    <div className='flex flex-col lg:flex-row gap-6  w-full justify-between'>
+                    <div className='flex flex-col lg:flex-row gap-6 w-full justify-between'>
                         <div className='flex gap-4 lg:gap-10 h-full'>
-                            <Image width={100} height={100} className='lg:w-32 lg:h-32 w-20 h-20 object-cover object-center rounded-2xl ' src={websiteDetails?.imageUrl ?? "/"} alt='' />
+                            <Image width={100} height={100} className='lg:w-32 lg:h-32 w-20 h-20 object-cover object-center rounded-2xl' src={websiteDetails?.imageUrl ?? "/"} alt='' />
                             <div className='flex flex-col items-center'>
                                 <h1 className='text-3xl lg:text-6xl font-sai my-2 text-white'>
-                                    <span className='font-kumar text-yellow-600'>#</span> {websiteDetails?.websiteName}  </h1>
-                                <p className='text-2xl hidden lg:block text-white  font-light'>{websiteDetails?.tagline}</p>
+                                    <span className='font-kumar text-yellow-600'>#</span> {websiteDetails?.websiteName}
+                                </h1>
+                                <p className='text-2xl hidden lg:block text-white font-light'>{websiteDetails?.tagline}</p>
                             </div>
                         </div>
-                        <p className='text-2xl lg:hidden  text-white  font-light'>{websiteDetails?.tagline}</p>
-                        <div className=' flex flex-col lg:flex-row  items-center gap-4'>
-                            <div 
-                            className=' lg:w-[240px] w-full justify-center text-xl flex  items-center gap-4 font-semibold rounded-md border-2 border-white text-white p-4 '>
-                               
-                                <p> visits :{data?.visitors}</p>
+                        <p className='text-2xl lg:hidden text-white font-light'>{websiteDetails?.tagline}</p>
+                        <div className='flex flex-col lg:flex-row items-center gap-4'>
+                            <div className='lg:w-[240px] w-full justify-center text-xl flex items-center gap-4 font-semibold rounded-md border-2 border-white text-white p-4'>
+                                <p>Visits: {data?.visitors}</p>
                             </div>
                             <Link href={websiteDetails?.websiteUrl ?? "/error"} className='text-xl font-semibold w-full lg:w-[200px] text-center rounded-md bg-white text-gray-900 p-4 font-sai'>Visit Website</Link>
                         </div>
                     </div>
                 </div>
-                <hr className='w-full mt-10 ' />
-                <div className='my-10 '>
-                    <h1 className='text-white font-sai text-3xl'> what is {websiteDetails?.websiteName} ?</h1>
+                <hr className='w-full mt-10' />
+                <div className='my-10'>
+                    <h1 className='text-white font-sai text-3xl'>What is {websiteDetails?.websiteName}?</h1>
                     <p className='py-4 text-white text-xl whitespace-pre-line'>
                         {websiteDetails?.description}
                     </p>
