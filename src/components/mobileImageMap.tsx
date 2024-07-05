e250ffc462161ffc57debe394b6243aebe8a355d
banala sai sathwik <banalasaisathwik@gmail.com>
2024-07-05 23:01:32 +0530

    mobile view bug cleared

--- src/components/mobileImageMap.tsx
+++ src/components/mobileImageMap.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import { motion } from "framer-motion";
 
-const ImageMap: React.FC = () => {
+const MobileimageMap: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [mapLoaded, setMapLoaded] = useState<boolean>(false);
     const [isExpanded, setIsExpanded] = useState<boolean | null>(null);
@@ -30,14 +30,6 @@ const ImageMap: React.FC = () => {
         setIsExpanded(sessionIsExpanded === 'true' ? true : false);
     }, []);
 
-    const handlePixelClick = (row: number, col: number): void => {
-        void router.push(`/pixel?row=${row}&col=${col}`);
-    };
-
-    const toggleExpand = (): void => {
-        sessionStorage.setItem('isExpanded', 'true')
-        setIsExpanded(prevState => !prevState);
-    };
 
     const downloadMap = () => {
         const canvas = canvasRef.current;
@@ -69,7 +61,7 @@ const ImageMap: React.FC = () => {
             // Here, we're using the unscaled mouse positions for simplicity.
             setTooltip({
                 visible: true,
-                content: `Pixel: (${row}, ${col})`,
+                content: `Pixel: (${row}, ${col}) - Click here for more details`,
                 x: e.clientX - rect.left, // Position tooltip relative to canvas, not viewport
                 y: e.clientY - rect.top
             });
@@ -79,77 +71,36 @@ const ImageMap: React.FC = () => {
         setTooltip(prev => ({ ...prev, visible: false }));
     };
 
-    const variants = {
-        normal: {
-            y: 0,
-            scale: 0.98,
-        },
-        expanded: {
-            y: -50,
-            scale: 1,
-        },
-    };
-
     return (
-        <motion.div
-            initial={false}
-            animate={isExpanded !== null ? (isExpanded ? 'expanded' : 'normal') : undefined}
-            variants={variants}
-            className='cursor-pointer  w-full min-h-screen overflow-y-auto'
-            onClick={toggleExpand}
-        >
-
+        <div className='cursor-pointer md:hidden flex justify-center items-center w-full min-h-screen overflow-y-auto'>
             <canvas
-                className={`cursor-pointer ${isExpanded ? 'mt-[200px]' : 'absolute top-1/2 translate-y-[-40%] left-1/2 translate-x-[-50%] w-[60%] lg:w-[20%] '}  transition-all duration-300`}
+                className='cursor-pointer w-full h-full object-contain transition-all duration-300'
                 ref={canvasRef}
                 width={2000}
                 height={2000}
-                onClick={(e) => {
-                    if (isExpanded && mapLoaded) {
-                        const rect = e.currentTarget.getBoundingClientRect();
-                        const x = e.clientX - rect.left;
-                        const y = e.clientY - rect.top;
-                        const row = Math.floor(y / 10);
-                        const col = Math.floor(x / 10);
-                        handlePixelClick(row, col);
-                    }
+                onClick={() => {
+                    void router.push(`/pixel?row=0&col=0`);
                 }}
                 onMouseMove={handleMouseMove}
                 onMouseLeave={handleMouseLeave}
-
             />
             {tooltip.visible && canvasRef.current && (
-                <div
-                    style={{
-                        position: 'absolute',
-                        top: `${tooltip.y + canvasRef.current.offsetTop + 15}px`,
-                        left: `${tooltip.x + canvasRef.current.offsetLeft + 15}px`,
-                        backgroundColor: 'white',
-                        padding: '5px',
-                        borderRadius: '5px',
-                        boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
-                        zIndex: 100,
-                    }}
-                >
+                <div>
                     {tooltip.content}
                 </div>
             )}
-
-
-            {!isExpanded && (
+            {true && (
                 <p className="text-gray-700 absolute top-1/2 left-[50%] transform translate-x-[-100%] animate-pulse -translate-y-[-0%]">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-14 h-14">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672L13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
                     </svg>
-
                 </p>
             )}
-
             <button className="absolute bottom-10 right-10 bg-white text-gray-900 px-4 py-2 rounded shadow" onClick={downloadMap}>
                 Download Map
             </button>
-        </motion.div>
+        </div>
     );
 };
 
-export default ImageMap;
+export default MobileimageMap;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ImageMap from '../components/ImageMap';
-
+import MobileimageMap from '~/components/mobileImageMap';
+import Loading from '~/components/Loading';
 
 const Home: React.FC = () => {
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
 
+  useEffect(() => {
+    const detectMobile = () => {
+      const userAgent = typeof navigator === 'undefined' ? '' : navigator.userAgent;
+      const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+      setIsMobile(mobile);
+    };
 
+    // Use a timeout to ensure the detection runs after the initial render
+    const timer = setTimeout(detectMobile, 100);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Render nothing or a loading state until the mobile detection is complete
+  if (isMobile === null) {
+    return <div> 
+      <Loading/>
+    </div>;
+  }
+
+  if (isMobile) {
+    return (
+      <div className='bg-center min-h-screen w-full flex flex-col justify-center items-center'>
+        <MobileimageMap/>
+      </div>
+    );
+  }
 
   return (
-    <div className='  bg-center min-h-screen w-full flex flex-col justify-center items-center'>
-      
+    <div className='bg-center min-h-screen w-full flex flex-col justify-center items-center'>
       <ImageMap />
-
     </div>
   );
 };
