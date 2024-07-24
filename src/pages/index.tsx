d7f54301cc209ac02691138d2a4e4fc5e2050ca1
banala sai sathwik <banalasaisathwik@gmail.com>
2024-07-24 13:47:19 +0530

    isMobile package downloaded

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from 'react';
 import ImageMap from '../components/ImageMap';
 import MobileimageMap from '~/components/mobileImageMap';
 import Loading from '~/components/Loading';
+import isMobile from 'is-mobile';
+
 
 const Home: React.FC = () => {
-  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+  const [IsMobileDevice, setIsMobileDevice] = useState<boolean | null>(null);
 
   useEffect(() => {
     const detectMobile = () => {
-      const userAgent = typeof navigator === 'undefined' ? '' : navigator.userAgent;
-      const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-      setIsMobile(mobile);
+      const mobile = isMobile();
+      setIsMobileDevice(mobile);
     };
+
 
     // Use a timeout to ensure the detection runs after the initial render
     const timer = setTimeout(detectMobile, 1000);
@@ -26,7 +28,7 @@ const Home: React.FC = () => {
     </div>;
   }
 
-  if (isMobile) {
+  if (IsMobileDevice) {
     return (
       <div className='bg-center min-h-screen w-full flex flex-col justify-center items-center'>
         <MobileimageMap/>
